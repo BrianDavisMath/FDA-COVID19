87ef61c45811c9e086d5cd321ffbda2e42dbf76e
Rick Fulton <rick@MacBook-Pro-2.local>
2020-03-22 20:54:25 -0700

    added saving and loading to random forest model

--- feature_selection/feature_selection.py
+++ feature_selection/feature_selection.py
@@ -10,9 +10,27 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from deap import base
 from deap import creator
 from deap import tools
+from abc import ABC, abstractmethod
+
+class FeatureSelectionModel(ABC):
+    @abstractmethod
+    def fit(self, features, labels, num_trials=25, randseed=42):
+        pass
+
+    @abstractmethod
+    def transform(self, features, num_keep_features):
+        pass
+
+    @abstractmethod
+    def save(self, location):
+        pass
+
+    @abstractmethod
+    def load(self, location):
+        pass
 
 
-class RandomForestFeatureSelection:
+class RandomForestFeatureSelection(FeatureSelectionModel):
     def __init__(self):
         self.feature_importance = None
 
@@ -28,11 +46,16 @@ class RandomForestFeatureSelection:
     def transform(self, features, num_keep_features):
         return np.take(features, np.argsort(self.feature_importance)[:num_keep_features], axis=1)
 
-    def save(self):
-        print("save")
+    def save(self, location, serialized_feature_selector=None):
+        pickle_out = open(location,"wb")
+        pickle.dump(self, pickle_out)
+        pickle_out.close()
 
-    def load(self):
-        print("load")
+    def load(self, location):
+        #serialized_fs = super(self, location)
+        serialized_fs = open(location, 'rb')
+        fs = pickle.load(serialized_fs)
+        self.feature_importance = fs.feature_importance
 
 
 """
@@ -191,10 +214,14 @@ input_array = np.random.sample(size=(n_samples, n_dims))
 activity_labels = np.random.choice(2, size=n_samples, p=(0.95, 0.05))
 
 # Random Forest Method: usage example
+model_location = 'example_model'
+
 feature_selector = RandomForestFeatureSelection()
 feature_selector.fit(input_array, activity_labels)
-s = pickle.dumps(feature_selector)
-feature_selector2 = pickle.loads(s)
+feature_selector.save('example_model')
+
+feature_selector2 = RandomForestFeatureSelection()
+feature_selector2.load(model_location)
 reduced_features = feature_selector2.transform(input_array, 10)
 print(reduced_features)
 
