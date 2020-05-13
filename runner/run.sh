b4e1dbee6a60bba2e6b64283f38f2cb578aa0c75
Greg Ross <gregxross@gmail.com>
2020-05-13 11:05:12 -0700

    Modified the bash script ready to run on HPC.

--- runner/run.sh
+++ runner/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-python job.py -a 0.02 -s 0.01 -f test_data/ -d False -t False -n 'no_weights'
+python job.py -a 0.01 -s 0.01 -d False -t False -n 'no_weights'
 
-python job.py -a 0.02 -s 0.01 -f test_data/ -d True -t False -n 'dim_red_weights'
+python job.py -a 0.01 -s 0.01 -d True -t False -n 'dim_red_weights'
 
-python job.py -a 0.02 -s 0.01 -f test_data/ -d False -t True -n 'training_weights'
+python job.py -a 0.01 -s 0.01 -d False -t True -n 'training_weights'
 
-python job.py -a 0.02 -s 0.01 -f test_data/ -d True -t True -n 'all_weights'
+python job.py -a 0.01 -s 0.01 -d True -t True -n 'all_weights'
