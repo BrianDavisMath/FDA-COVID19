09227e7dcbc024f8556dc61c32d4eaeef6442a8b
Greg Ross <gregxross@gmail.com>
2020-06-17 11:02:07 -0700

    Updated run script for correlation thresholds.

--- runner/run.sh
+++ runner/run.sh
@@ -2,10 +2,10 @@
 
 python job.py -a 0.75 -s 0.05 -m 0.35 -z 0.0 -d False -t False -n '1'
 
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 5.0 -d False -t False -n '2'
+python job.py -a 0.75 -s 0.05 -m 0.35 -z 1.0 -d False -t False -n '2'
 
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 10.0 -d False -t False -n '3'
+python job.py -a 0.75 -s 0.05 -m 0.35 -z 2.0 -d False -t False -n '3'
 
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 15.0 -d False -t False -n '4'
+python job.py -a 0.75 -s 0.05 -m 0.35 -z 3.0 -d False -t False -n '4'
 
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 20.0 -d False -t False -n '5'
+python job.py -a 0.75 -s 0.05 -m 0.35 -z 4.0 -d False -t False -n '5'
