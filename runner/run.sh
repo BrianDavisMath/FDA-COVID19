02e4414c254fe7fedcd72b57c947e86c9eb4ab9b
Greg Ross <gregxross@gmail.com>
2020-07-20 09:55:02 -0700

    Updated run script.

--- runner/run.sh
+++ runner/run.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 0.0 -d False -t False -n '1'
-
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 1.0 -d False -t False -n '2'
-
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 2.0 -d False -t False -n '3'
-
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 3.0 -d False -t False -n '4'
-
-python job.py -a 0.75 -s 0.05 -m 0.35 -z 4.0 -d False -t False -n '5'
+python job.py -d False -n '1'
